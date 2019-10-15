97ef17a951388837aa849aa2bff012f5ec9e3996
Mike Shibodaev <witcher759@gmail.com>
2019-10-15 19:45:53 +0400

    Последние изминения

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,19 @@
 import './vendor';
+
+;(function () {
+    var rangeClock = document.querySelector('.meter-clock');
+    var userInfo = document.querySelector('user-info');
+    var userInfo = document.querySelector('user-info');
+
+
+
+})();
+
+// function rangeChange() {
+//   var rotateClock = rangeMeter.value;
+
+//   rangeClock.style.transform = 'rotate(' + (-90 + ((rotateClock * 180) / 100)) + 'deg)';
+//   rangeShow.value = rotateClock + '%';
+// }
+
+//   rangeMeter.addEventListener('input', rangeChange);
